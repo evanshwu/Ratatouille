95b0aef0d660cfd9c11729cf9e77afdd5949ef33
Hsin-Han Wu <evan.hsinhanwu@gmail.com>
2019-12-09 13:11:45 -0800

    Code optimization
    Remove CalorieCalculator

--- src/main/java/com/cmu/ratatouille/utils/CalorieCalculator.java
+++
@@ -1,9 +0,0 @@
-package com.cmu.ratatouille.utils;
-
-public class CalorieCalculator {
-    public static int calculateBmr(String gender, int weight, int height, int age){
-        if(gender.equals("m"))
-            return (int)(10 * weight + 6.25 * height - 5 * age +5);
-        return (int)(10 * weight + 6.25 * height - 5 * age - 161);
-    }
-}
